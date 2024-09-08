d9f80d2497f53b7991b1cd7458a0473472f8aaa6
Falco90 <falcorodenburg@gmail.com>
2024-09-08 18:27:49 +0700

    add conditional data storing depending on query

--- src/hypersync.rs
+++ src/hypersync.rs
@@ -108,30 +108,9 @@ pub async fn query<'a>(app: &mut App<'a>) {
                         if let Ok(decoded_log) = decoder.decode_log(&log) {
                             match &decoded_log {
                                 Some(_) => {
-                                    let decoded_log = decoded_log.unwrap();
-                                    app.transfers.erc20_transfers.push(Erc20Transfer {
-                                        hash: log.transaction_hash.unwrap().encode_hex(),
-                                        block: log.block_number.unwrap().to_string(),
-                                        contract: log.address.unwrap().encode_hex(),
-                                        from: decoded_log.indexed[0]
-                                            .as_address()
-                                            .unwrap()
-                                            .to_string(),
-                                        to: decoded_log.indexed[1]
-                                            .as_address()
-                                            .unwrap()
-                                            .to_string(),
-                                        amount: decoded_log.body[0]
-                                            .as_uint()
-                                            .unwrap()
-                                            .0
-                                            .to_string(),
-                                    });
-                                }
-                                None => {
-                                    if let Ok(decoded_log) = erc721_decoder.decode_log(&log) {
+                                    if app.query.erc20_transfers {
                                         let decoded_log = decoded_log.unwrap();
-                                        app.transfers.erc721_transfers.push(Erc721Transfer {
+                                        app.transfers.erc20_transfers.push(Erc20Transfer {
                                             hash: log.transaction_hash.unwrap().encode_hex(),
                                             block: log.block_number.unwrap().to_string(),
                                             contract: log.address.unwrap().encode_hex(),
@@ -143,12 +122,37 @@ pub async fn query<'a>(app: &mut App<'a>) {
                                                 .as_address()
                                                 .unwrap()
                                                 .to_string(),
-                                            token_id: decoded_log.indexed[2]
+                                            amount: decoded_log.body[0]
                                                 .as_uint()
                                                 .unwrap()
                                                 .0
                                                 .to_string(),
                                         });
+                                    }
+                                }
+                                None => {
+                                    if app.query.erc721_transfers {
+                                        if let Ok(decoded_log) = erc721_decoder.decode_log(&log) {
+                                            let decoded_log = decoded_log.unwrap();
+                                            app.transfers.erc721_transfers.push(Erc721Transfer {
+                                                hash: log.transaction_hash.unwrap().encode_hex(),
+                                                block: log.block_number.unwrap().to_string(),
+                                                contract: log.address.unwrap().encode_hex(),
+                                                from: decoded_log.indexed[0]
+                                                    .as_address()
+                                                    .unwrap()
+                                                    .to_string(),
+                                                to: decoded_log.indexed[1]
+                                                    .as_address()
+                                                    .unwrap()
+                                                    .to_string(),
+                                                token_id: decoded_log.indexed[2]
+                                                    .as_uint()
+                                                    .unwrap()
+                                                    .0
+                                                    .to_string(),
+                                            });
+                                        }
                                     }
                                 }
                             }
@@ -160,23 +164,25 @@ pub async fn query<'a>(app: &mut App<'a>) {
         }
 
         for batch in res.data.transactions {
-            for tx in batch {
-                let regular_transfer = RegularTransfer {
-                    hash: tx.hash.unwrap().encode_hex(),
-                    block_hash: tx.block_hash.unwrap().encode_hex(),
-                    block: tx.block_number.unwrap().to_string(),
-                    nonce: format_ether(U256::from(tx.nonce.unwrap().as_ref())),
-                    from: tx.from.unwrap().encode_hex(),
-                    to: tx.to.unwrap().encode_hex(),
-                    value: format_ether(U256::from(tx.value.unwrap().as_ref())),
-                    gas_used: format_ether(U256::from(tx.gas_used.unwrap().as_ref())),
-                };
-                let parsed_value = regular_transfer.value.as_str().parse::<f64>().unwrap();
-                if (regular_transfer.from.to_lowercase() == app.query.address.to_lowercase()
-                    || regular_transfer.to.to_lowercase() == app.query.address.to_lowercase())
-                    && parsed_value > 0.0000
-                {
-                    app.transfers.regular_transfers.push(regular_transfer);
+            if app.query.regular_transfers {
+                for tx in batch {
+                    let regular_transfer = RegularTransfer {
+                        hash: tx.hash.unwrap().encode_hex(),
+                        block_hash: tx.block_hash.unwrap().encode_hex(),
+                        block: tx.block_number.unwrap().to_string(),
+                        nonce: format_ether(U256::from(tx.nonce.unwrap().as_ref())),
+                        from: tx.from.unwrap().encode_hex(),
+                        to: tx.to.unwrap().encode_hex(),
+                        value: format_ether(U256::from(tx.value.unwrap().as_ref())),
+                        gas_used: format_ether(U256::from(tx.gas_used.unwrap().as_ref())),
+                    };
+                    let parsed_value = regular_transfer.value.as_str().parse::<f64>().unwrap();
+                    if (regular_transfer.from.to_lowercase() == app.query.address.to_lowercase()
+                        || regular_transfer.to.to_lowercase() == app.query.address.to_lowercase())
+                        && parsed_value > 0.0000
+                    {
+                        app.transfers.regular_transfers.push(regular_transfer);
+                    }
                 }
             }
         }
